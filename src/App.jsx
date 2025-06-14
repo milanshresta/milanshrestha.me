e1bbdea981017033edcd139dca84d1206acad18e
Milan Shrestha <milanaslo@Milans-MacBook-Pro.local>
2025-06-15 02:54:49 +0545

    portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hi, It's me Milan Shrestha.</h1>
+    </>
+  )
+}
+
+export default App
